18ee2394f245e750430603aa3f2edb5577531878
eric liu <54070356@qq.com>
2019-04-14 17:52:53 +0800

    initial

---
+++ akka-quickstart-java/src/test/java/com/lightbend/akka/sample/AkkaQuickstartTest.java
@@ -0,0 +1,40 @@
+package com.lightbend.akka.sample;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.quickstart.Greeter;
+import akka.quickstart.Greeter.Greet;
+import akka.quickstart.Greeter.WhoToGreet;
+import akka.quickstart.Printer.Greeting;
+import akka.testkit.javadsl.TestKit;
+
+public class AkkaQuickstartTest {
+    static ActorSystem system;
+
+    @BeforeClass
+    public static void setup() {
+        system = ActorSystem.create();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        TestKit.shutdownActorSystem(system);
+        system = null;
+    }
+
+    @Test
+    public void testGreeterActorSendingOfGreeting() {
+        final TestKit testProbe = new TestKit(system);
+        final ActorRef helloGreeter = system.actorOf(Greeter.props("Hello", testProbe.getRef()));
+        helloGreeter.tell(new WhoToGreet("Akka"), ActorRef.noSender());
+        helloGreeter.tell(new Greet(), ActorRef.noSender());
+        Greeting greeting = testProbe.expectMsgClass(Greeting.class);
+        assertEquals("Hello, Akka", greeting.message);
+    }
+}
